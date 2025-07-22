e479492bca996ffe9f483121d4c5d86aa9371786
Alessandra <alessaconceicao501501@gmail.com>
2025-07-21 21:17:18 -0400

    feat: adicionado imagem no e-mail (caso exista)

--- services/email.py
+++ services/email.py
@@ -1,16 +1,18 @@
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
 import os
 from dotenv import load_dotenv
+from email import encoders
 
 load_dotenv()
 
-def send_capsule_email(to_email: str, subject: str, body: str):
+def send_capsule_email(to_email: str, subject: str, body: str, image_path: str = None ):
     smtp_server = os.getenv("SMTP_SERVER")
     smtp_port = int(os.getenv("SMTP_PORT"))
     smtp_user = os.getenv("SMTP_USER")
-    smtp_password = os.getenv("SMT_PASSWORD")
+    smtp_password = os.getenv("SMTP_PASSWORD")
     
     msg = MIMEMultipart()
     msg["From"] = smtp_user
@@ -18,6 +20,14 @@ def send_capsule_email(to_email: str, subject: str, body: str):
     msg["Subject"] = subject
     
     msg.attach(MIMEText(body, "plain"))
+    
+    if image_path and os.path.exists(image_path):
+        with open(image_path, 'rb') as file:
+            mime = MIMEBase('application', 'octet-stream')
+            mime.set_payload(file.read())
+            encoders.encode_base64(mime)
+            mime.add_header('Content_Disposition', f'attachment; filename="{os.path.basename(image_path)}"')
+            msg.attach(mime)
     
     with smtplib.SMTP_SSL(smtp_server, smtp_port) as server:
         server.login(smtp_user, smtp_password)
